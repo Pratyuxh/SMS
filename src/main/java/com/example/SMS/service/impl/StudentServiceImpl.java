b447621a699d7e9a0aa56c9f14c550ebe71d1da8
Pratyush Sharma <sharmapratyush_bt2k16@dtu.ac.in>
2023-02-08 17:56:18 +0530

    Initial commit

---
+++ src/main/java/com/example/SMS/service/impl/StudentServiceImpl.java
@@ -0,0 +1,45 @@
+package com.example.SMS.service.impl;
+
+import com.example.SMS.entity.Student;
+import com.example.SMS.repository.StudentRepository;
+import com.example.SMS.service.StudentService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+
+    private StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        super();
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return StudentRepository.findAll();
+    }
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public Student getStudentById(Long id) {
+        return studentRepository.findById(id).get();
+    }
+
+    @Override
+    public Student updateStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public void deleteStudentById(Long id) {
+        studentRepository.deleteById(id);
+    }
+
+}
